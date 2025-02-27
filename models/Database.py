85adad1ae0d564a2f2b51694198798f435cc81ac
Eveli <estahelstrauchpulk@gmail.com>
2025-02-27 10:09:58 +0200

    Kui andmebaasi pole, luuakse andmebaas ja fail words.

--- models/Database.py
+++ models/Database.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import sys
+from tkinter import messagebox
 
 
 class Database:
@@ -10,7 +11,7 @@ class Database:
     def __init__(self):
 
         if not os.path.exists(self.db_name):
-            raise FileNotFoundError('Andmebaasi ei leitud')
+            self.make_database()
 
         """Konstruktor"""
         self.conn = sqlite3.connect(self.db_name)
@@ -20,6 +21,26 @@ class Database:
         self.check_table()
 
         self.get_categories()
+
+    def make_database(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        try:
+            sql = '''CREATE TABLE "words" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"word"	TEXT NOT NULL,
+	"category"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)'''
+            print("Attempting to create table...")
+            self.cursor.execute(sql)
+            self.conn.commit()
+            messagebox.showinfo('Loodi andmebaas', 'Andmebaas koos tabeliga "words" loodi projekti Hangman_2025_DB_Manage peakausta!')
+        except sqlite3.OperationalError as error:
+            print(f'Tabeli loomisel tekkis viga: {error}')
+        finally:
+            print('Tubli!')
+
 
     def check_table(self):
         if self.cursor:
@@ -37,7 +58,7 @@ class Database:
                         print('Veerge ei ole olemas.')
                 else:
                     print('Tabelit word pole')
-                    self.close_connection()
+                    #self.close_connection()
             except sqlite3.Error as error:
                 print(f'Ei saanud ühendust luua: {error} ')
                 sys.exit(1)
@@ -55,8 +76,6 @@ class Database:
             print(f'Tõrge andmebaasi ühenduse loomisel:{error}')
             self.conn = None
             self.cursor = None
-
-
 
     def get_categories(self):
         if self.cursor:
@@ -141,5 +160,4 @@ class Database:
                 print(f'Ühendus andmebaasiga {self.db_name} suletud.')
         except sqlite3.Error as error:
             print(f'Tõrge ühenduse sulgemisel: {error}')
-
 
